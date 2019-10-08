566bdab0c939407d7e6f1a42912a7db48736bfcc
cryptorootz <cryptorootz@protonmail.com>
2019-10-08 13:14:10 -0500

    upgrade

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -130,3 +130,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.protonmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'cryptorootz@protonmail.com'
+EMAIL_HOST_PASSWORD = 'DreadlockMan89'
